2520f967cf8e16733a19d43f059211bf2755e3b9
andyboez <andyboez13@gmail.com>
2015-06-14 18:02:43 -0400

    Update gigapixel.py

--- gigapixel.py
+++ gigapixel.py
@@ -351,7 +351,7 @@ buttons = [
    Button((260,120, 60, 60), bg='cog',   cb=valuesCallback, value=3),
    Button((  0,180,160, 60), bg='ok',    cb=valuesCallback, value=-1),
    Button((160,180, 70, 60), bg='left',  cb=motorCallback, value=1),
-   Button((230,180, 70, 60), bg='right', cb=motorCallback, value=2)],
+   Button((230,180, 70, 60), bg='right', cb=motorCallback, value=3)],
 
   # Screen 2 for numeric input
   [Button((  0,  0,320, 60), bg='box'),
